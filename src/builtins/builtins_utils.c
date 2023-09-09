e65a668d950c2431e56daf1372036f1f80c1fef8
Migueldar <miguelarp3907@gmail.com>
2023-09-09 02:34:39 +0200

    exxexc

--- src/builtins/builtins_utils.c
+++ src/builtins/builtins_utils.c
@@ -6,7 +6,7 @@
 /*   By: mde-arpe <mde-arpe@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 07:32:55 by mde-arpe          #+#    #+#             */
-/*   Updated: 2023/09/02 08:12:45 by mde-arpe         ###   ########.fr       */
+/*   Updated: 2023/09/09 02:33:45 by mde-arpe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,24 @@ int	is_builtin(char *cmd)
 	return (0);
 }
 
+// take into account side effects that executing 
+// exit in children may have for the global variable
 int	execute_builtin(t_command_l *cmd, t_env **env)
 {
-	(void) cmd;
-	(void) env;
-
-	printf("in builtin\n");
+	// redirs
+	if (!ft_strncmp_ignore_case(cmd->cmd->args->content, "echo", 5))
+		return (ft_echo(cmd->cmd->args->next));
+	if (!ft_strncmp_ignore_case(cmd->cmd->args->content, "cd", 3))
+		return (ft_cd(*env, cmd->cmd->args->next));
+	if (!ft_strncmp_ignore_case(cmd->cmd->args->content, "pwd", 4))
+		return (ft_pwd());
+	if (!ft_strncmp_ignore_case(cmd->cmd->args->content, "export", 7))
+		return (ft_pwd());
+	if (!ft_strncmp_ignore_case(cmd->cmd->args->content, "unset", 6))
+		return (ft_unset());
+	if (!ft_strncmp_ignore_case(cmd->cmd->args->content, "env", 4))
+		return (ft_env(*env));
+	if (!ft_strncmp_ignore_case(cmd->cmd->args->content, "exit", 5))
+		return (ft_pwd());
 	return (0);
 }
