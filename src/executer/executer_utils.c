3879b7919d8c3fd52a668c792a362a10b1ed4f7c
luciamar1 <luciajimenamartinrodriguez@gmail.com>
2023-09-15 14:52:12 +0200

    redo of single fork before pull of master

--- src/executer/executer_utils.c
+++ src/executer/executer_utils.c
@@ -6,7 +6,7 @@
 /*   By: lucia-ma <lucia-ma@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 23:18:16 by mde-arpe          #+#    #+#             */
-/*   Updated: 2023/09/14 19:54:36 by lucia-ma         ###   ########.fr       */
+/*   Updated: 2023/09/15 14:50:14 by lucia-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,18 +35,18 @@ t_command	*fork_free_command_l(t_command_l **command_l, int which)
 }
 
 
-int	single_forked_cmd(t_list *pids)
+int	single_forked_cmd(t_command_l *cmd)
 {
-	t_list	*new;
 	int		pid;
 
 	pid = fork();
-	printf("%d\n", pid);
 	if (pid < 0)
 		return (1);
-	new = ft_lstnew(pids);
-	if (!new)
-		return (1);
-	ft_lstadd_back(&pids, new);
+	if (pid > 0)
+	{
+		
+	}
+	else
+		wait(NULL);
 	return (0);
 }
