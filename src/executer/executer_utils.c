a7a36097315a4d896855c00eb41cd8409164eee6
luciamar1 <luciajimenamartinrodriguez@gmail.com>
2023-09-20 15:06:22 +0200

    separate childs_tasks

--- src/executer/executer_utils.c
+++ src/executer/executer_utils.c
@@ -6,7 +6,7 @@
 /*   By: lucia-ma <lucia-ma@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 23:18:16 by mde-arpe          #+#    #+#             */
-/*   Updated: 2023/09/20 14:24:39 by lucia-ma         ###   ########.fr       */
+/*   Updated: 2023/09/20 14:51:55 by lucia-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,37 +34,48 @@ t_command	*fork_free_command_l(t_command_l **command_l, int which)
 	return (fork_command);
 }
 
+int	childs_tasks(t_env **env, t_command_l *cmd)
+{
+	char *programPath = "/bin/ls";
+	char *args[] = {programPath, "-l", "-a", "/bin", NULL};
+	char **envi;
+
+	envi = env_to_array(*env);
+	if (!envi)
+		return (1);
+	if (handle_redirs(cmd->cmd->redirs))
+		return(1);
+	// printf("redir == %s\n", cmd->cmd->redirs->redir->where);
+	printf("soy un hijo\n");
+	ft_lstclear_cmd_l(&cmd);
+	ft_lstclear((t_list **) env, (void (*)(void *)) free_env_var);
+	clear_history();
+	if (execve(programPath, args, envi) == -1)
+	{
+		perror("minishell"); // Si ocurre un error, imprime un mensaje de error
+		exit(1);
+    }
+	return (0);
+}
 
 int	single_forked_cmd(t_env **env, t_command_l *cmd)
 {
 	printf("antes\n");
 	int		pid;
-	char *programPath = "/bin/ls";
-	char *args[] = {programPath, "-l", "-a", "/bin", NULL};
-	char **envi;
 	//if (find path(env, cmd->cmd->args->content)
 	//{
-		envi = env_to_array(*env);
-		if (!envi)
-			return (1);
 		pid = fork();
 		if (pid < 0)
 			return (1);
 		if (pid == 0)
 		{
 			//redirecciones
-			if (handle_redirs(cmd->cmd->redirs))
-				return(1);
-			// printf("redir == %s\n", cmd->cmd->redirs->redir->where);
-			printf("soy un hijo\n");
-			if (execve(programPath, args, envi) == -1)
+			if (childs_tasks(env, cmd))
 			{
-				perror("minishell"); // Si ocurre un error, imprime un mensaje de error
-        		exit(1);
-    		}
-			ft_lstclear_cmd_l(&cmd);
-			ft_lstclear((t_list **) env, (void (*)(void *)) free_env_var);
-			clear_history();
+				ft_lstclear_cmd_l(&cmd);
+				ft_lstclear((t_list **) env, (void (*)(void *)) free_env_var);
+				clear_history();
+			}
 			//ejecutar
 			//if no se ha podido verificar liberamos ¿en padre?
 		}
