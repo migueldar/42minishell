2b6c4e602c1f6f1f2d94bbd203a23472e93aca9f
luciamar1 <luciajimenamartinrodriguez@gmail.com>
2023-09-20 15:11:52 +0200

    k

--- src/executer/executer_utils.c
+++ src/executer/executer_utils.c
@@ -6,7 +6,7 @@
 /*   By: lucia-ma <lucia-ma@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 23:18:16 by mde-arpe          #+#    #+#             */
-/*   Updated: 2023/09/20 14:40:09 by lucia-ma         ###   ########.fr       */
+/*   Updated: 2023/09/20 15:10:04 by lucia-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,11 @@ t_command	*fork_free_command_l(t_command_l **command_l, int which)
 	return (fork_command);
 }
 
-int	manage_pipes_and_forks()
+int	manage_pipes_and_forks(t_env **env, t_command_l	*cmd)
 {
-	
+	while (cmd)
+	{
+		fork();
+		cmd = cmd ++;
+	}
 }
