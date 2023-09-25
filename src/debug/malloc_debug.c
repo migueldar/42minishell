d96aa7a2d1c11130da2d397efed5ca4b435dc61f
Migueldar <miguelarp3907@gmail.com>
2023-09-25 17:56:02 +0200

    done

--- src/debug/malloc_debug.c
+++ src/debug/malloc_debug.c
@@ -6,7 +6,7 @@
 /*   By: mde-arpe <mde-arpe@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 03:25:57 by mde-arpe          #+#    #+#             */
-/*   Updated: 2023/09/18 18:29:21 by mde-arpe         ###   ########.fr       */
+/*   Updated: 2023/09/25 17:34:34 by mde-arpe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,76 @@
 #include "minishell.h"
 #include "malloc_debug.h"
 
+static int	size_of_int(int n)
+{
+	int	counter;
+
+	counter = 0;
+	if (n <= 0)
+		counter++;
+	while (n != 0)
+	{
+		n /= 10;
+		counter++;
+	}
+	return (counter);
+}
+
+static char	*itoa(int n)
+{
+	char	*ret;
+	char	*ret_cpy;
+	long	long_n;
+	int		div;
+
+	ret = calloc(size_of_int(n) + 1, 1);
+	if (ret == NULL)
+		return (NULL);
+	ret_cpy = ret;
+	long_n = n;
+	div = 1;
+	if (long_n < 0)
+	{
+		*ret = '-';
+		ret++;
+		long_n *= -1;
+	}
+	while (long_n / div > 9)
+		div *= 10;
+	while (div)
+	{
+		*(ret++) = '0' + (long_n / div) % 10;
+		div /= 10;
+	}
+	return (ret_cpy);
+}
+
+static char	*strjoin(char const *s1, char const *s2)
+{
+	char	*ret;
+	char	*ret_cpy;
+
+	if (!s1 || !s2)
+		return (NULL);
+	ret = calloc(ft_strlen(s1) + ft_strlen(s2) + 1, 1);
+	ret_cpy = ret;
+	if (ret == NULL)
+		return (NULL);
+	while (*s1 != 0)
+		*(ret++) = *(s1++);
+	while (*s2 != 0)
+		*(ret++) = *(s2++);
+	return (ret_cpy);
+}
+
 void leaks()
 {
-	char *itoaed = ft_itoa(getpid());
-	char *commmand = ft_strjoin("leaks -q ", itoaed);
+	char *itoaed = itoa(getpid());
+	char *command = strjoin("leaks -q ", itoaed);
 
 	free(itoaed);
-	free(commmand);
-	system("leaks -q minishell");
+	system(command);
+	free(command);
 }
 
 #ifdef MALLOC_DEBUG
