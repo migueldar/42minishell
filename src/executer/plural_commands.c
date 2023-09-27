075cd111560c0f0375f96f36e0e99abc69808507
Migueldar <miguelarp3907@gmail.com>
2023-09-27 15:45:02 +0200

    prepare to merge

--- src/executer/plural_commands.c
+++ src/executer/plural_commands.c
@@ -6,7 +6,7 @@
 /*   By: mde-arpe <mde-arpe@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 23:18:16 by mde-arpe          #+#    #+#             */
-/*   Updated: 2023/09/26 20:49:55 by mde-arpe         ###   ########.fr       */
+/*   Updated: 2023/09/27 15:34:10 by mde-arpe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,13 +44,18 @@ int	manage_child(t_env **env, t_command_l *cmd, int fdin, int fdout, int *pid, i
 
 	pid[counter] = fork();
 	if (pid[counter] < 0)
-		return (perror("minishell"), 1);
+		return (close(fdin), close(fdout), perror("minishell"), 1);
 	if (pid[counter] == 0)
 	{
 		if (handle_dups(fdin, fdout))
 			(clear_child(env, NULL, NULL, NULL), ft_lstclear_cmd_l(&cmd), exit(1));
 		to_exec = isolate_cmd(cmd, counter);
 		childs_tasks(env, to_exec);
+	}
+	if (pid[counter] > 0)
+	{
+		close(fdin);
+		close(fdout);
 	}
 	return (0);
 }
@@ -70,14 +75,12 @@ void	swap_pipes(int pipes[2][2])
 int	plural_commands(t_env **env, t_command_l *cmd)
 {
 	int	pipes[2][2];
-	int	len;
 	int	*pid;
 	int	stat;
 	int	counter;
 	int	stat_chi;
 
-	len = ft_lstsize((t_list *) cmd);
-	pid = malloc(sizeof(int) * len);
+	pid = malloc(sizeof(int) * ft_lstsize((t_list *) cmd));
 	if (!pid)
 		return (perror("minishell"), 1);
 	pipe(pipes[0]);
@@ -85,7 +88,7 @@ int	plural_commands(t_env **env, t_command_l *cmd)
 	if (stat_chi)
 		return (1);
 	counter = 1;
-	while (counter < len - 1)
+	while (counter < ft_lstsize((t_list *) cmd) - 1)
 	{
 		pipe(pipes[1]);
 		stat_chi = manage_child(env, cmd, pipes[0][0], pipes[1][1], pid, counter);
@@ -98,7 +101,7 @@ int	plural_commands(t_env **env, t_command_l *cmd)
 	counter++;
 	if (stat_chi)
 		return (kill_childs(pid, counter), 1);
-	stat = wait_all(pid, len);
+	stat = wait_all(pid, ft_lstsize((t_list *) cmd));
 	free(pid);
 	return(WEXITSTATUS(stat));
 }
